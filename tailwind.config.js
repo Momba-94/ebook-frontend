9878b1c775cb26ff2dd67a813ad79863bc1277a5
Momba-94 <maria.om.bandeira@gmail.com>
2026-03-31 16:38:42 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      // Aqui podes adicionar cores ou animações personalizadas no futuro
+    },
+  },
+  plugins: [],
+}
